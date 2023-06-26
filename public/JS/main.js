2315311110835e398eb96b4ae9e3def499ee76c9
FabrizioCmp <fabriziocampari01@gmail.com>
2023-06-26 22:26:01 +0200

    adds bootstrap

---
+++ public/JS/main.js
@@ -0,0 +1,22 @@
+const eventEl = document.getElementById('event')
+
+events = [
+    "raduno",
+    "party",
+    "rinfresco",
+    "incontro",
+    "concerto",
+    "appuntamento",
+    "evento"
+]
+
+let eventNumber = 0
+
+function changeEvent(){
+  eventEl.textContent = events[eventNumber++]
+  if (eventNumber == events.length) {
+    eventNumber = 0
+  }
+}
+
+setInterval(changeEvent, 1000)
